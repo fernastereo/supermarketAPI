d1cb8085094dcc7815c09fd7ef59b7d91dd5ab6f
fernastereo <fernandoecueto@gmail.com>
2020-08-24 11:20:13 -0500

    Implementing the Categories Service

---
+++ SupermarketAPI/Services/CategoryService.cs
@@ -0,0 +1,17 @@
+﻿using SupermarketAPI.Domain.Models;
+using SupermarketAPI.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SupermarketAPI.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<IEnumerable<Category>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
