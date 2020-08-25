9d655d91fcba4a5fdc3adea88d25aec962dd0c1a
fernastereo <fernandoecueto@gmail.com>
2020-08-24 19:30:43 -0500

    The Update Logic

--- SupermarketAPI/Persistence/Repositories/CategoryRepository.cs
+++ SupermarketAPI/Persistence/Repositories/CategoryRepository.cs
@@ -21,9 +21,19 @@ namespace SupermarketAPI.Persistence.Repositories
             await _context.Categories.AddAsync(category);
         }
 
+        public async Task<Category> FindByIdAsync(int id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
         public async Task<IEnumerable<Category>> ListAsync()
         {
             return await _context.Categories.ToListAsync();
+        }
+
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
         }
     }
 }
